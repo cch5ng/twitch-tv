1b1abf7a66b908e6e42df2b9805a02740f72b3d1
cch5ng <cch5ng@gmail.com>
2015-08-11 22:42:47 -0700

    now getting results for all of the channelNames elements. had to remove 2 names from the list because they are no longer active channels. also added one name to have an example channel that is always streaming. cleaned up comments

--- js/main.js
+++ js/main.js
@@ -1,57 +1,41 @@
 //better not to return from the xhr.done() function
 //why is the return always undefined??
 
-// var channel0 = "freecodecamp";
-// var channel1 = "medrybw";
-// var channel2 = 'Habathcx';
-
-var channelNames = ['freecodecamp', 'storbeck', 'terakilobyte', 'habathcx','RobotCaleb','comster404','brunofin','thomasballinger','noobs2ninjas','beohoff'];
+var channelNames = ['freecodecamp', 'storbeck', 'terakilobyte', 'habathcx','RobotCaleb','thomasballinger','noobs2ninjas','beohoff', 'medrybw']; 
+//the following channelNames were not recognized by twitch.tv
+//'brunofin', 'comster404',
 
 var channels = [];
 
 function isStreaming(channel) {
   var requestUrl = 'https://api.twitch.tv/kraken/streams/' + channel;// + '?callback=result';
-  var channelInfo;
   var channelObj = {};
       
   var jqxhr = $.ajax({
     url: requestUrl,
     dataType: 'jsonp'
   }).done(function(result) {
-    
     channelObj.name = channel;
 
-    if (result.stream !== null) {
-//       console.log(result.stream);
-//       console.log(result.stream.channel.status);
-      channelObj.streamStatus = result.stream.channel.status;
-      channelObj.url = result.stream.channel.url;          
-    } else {
+    if (result.stream === null) {
       channelObj.streamStatus = '';
       channelObj.url = 'http://www.twitch.tv/' + channel;
-//console.log(result.stream);
+    } else {
+      channelObj.streamStatus = result.stream.channel.status;
+      channelObj.url = result.stream.channel.url;
     }
     
     channels.push(channelObj);
-    //if (channels.length === channelNames.length) {
-      console.log(channels);          
-    //}
+    console.log(channels);
   }).fail(function(err) {
     console.log('error: ' + err);
   }).always(function() {
-    //console.log('completed');
-    //return channelInfo.stream;
-
+    if (channels.length === channelNames.length) {
+      console.log('finished');
+    }
   });
-            
 }
 
 channelNames.forEach(function(chan) {
 	isStreaming(chan);
 });
-//console.log(channels);
-
-
-// isStreaming(channel0);
-// isStreaming(channel1);
-// isStreaming(channel2);
