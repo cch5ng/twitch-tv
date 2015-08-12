31a7379aa63f9166c02640270314eec26fa6c440
cch5ng <cch5ng@gmail.com>
2015-08-12 00:19:06 -0700

    updated logic so content dynamically displayed from list of channel objects. added click listeners for online and offline headers (+ event handlers). fixed logic so that when headers are clicked, the list content is updated appropriately

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
-//better not to return from the xhr.done() function
-//why is the return always undefined??
 
 var channelNames = ['freecodecamp', 'storbeck', 'terakilobyte', 'habathcx','RobotCaleb','thomasballinger','noobs2ninjas','beohoff', 'medrybw']; 
 //the following channelNames were not recognized by twitch.tv
 //'brunofin', 'comster404',
 
 var channels = [];
+var state = 'streaming'; //'streaming' or 'notStreaming'
+
+function displayChannels() {
+
+
+	channels.forEach(function(chan) {
+		var divChannelsList = document.querySelector('.channelsList');
+		var p = document.createElement('p');
+		p.setAttribute('class', 'channel');
+
+		var htmlStr = "<a href='" + chan.url + "' target='_blank'>" + chan.name + "</a>";
+		var htmlSuffix;
+
+		if (state === 'streaming' && chan.streamStatus.length > 0) {
+				htmlSuffix = ' channel is streaming';
+				p.innerHTML = htmlStr + htmlSuffix;
+				divChannelsList.appendChild(p);
+		} else if (state === 'notStreaming' && chan.streamStatus.length === 0) {
+				htmlSuffix = ' channel is not streaming';
+				p.innerHTML = htmlStr + htmlSuffix;
+				divChannelsList.appendChild(p);
+		}
+	});
+}
 
 function isStreaming(channel) {
-  var requestUrl = 'https://api.twitch.tv/kraken/streams/' + channel;// + '?callback=result';
+  var requestUrl = 'https://api.twitch.tv/kraken/streams/' + channel;
   var channelObj = {};
       
   var jqxhr = $.ajax({
@@ -26,12 +48,13 @@ function isStreaming(channel) {
     }
     
     channels.push(channelObj);
-    console.log(channels);
+    //console.log(channels);
   }).fail(function(err) {
     console.log('error: ' + err);
   }).always(function() {
     if (channels.length === channelNames.length) {
       console.log('finished');
+      displayChannels();
     }
   });
 }
@@ -39,3 +62,33 @@ function isStreaming(channel) {
 channelNames.forEach(function(chan) {
 	isStreaming(chan);
 });
+
+//event handlers
+function onlineHeaderClickListener() {
+	console.log('online click');
+	if (state === 'notStreaming') {
+		state = 'streaming';
+		var divChannelsList = document.querySelector('.channelsList');
+		divChannelsList.innerHTML = '';
+
+		displayChannels();
+	}
+}
+
+function offlineHeaderClickListener() {
+	console.log('offline click');
+	if (state === 'streaming') {
+		state = 'notStreaming';
+		var divChannelsList = document.querySelector('.channelsList');
+		divChannelsList.innerHTML = '';
+
+		displayChannels();
+	}
+}
+
+//event listeners
+var onlineHeader = document.querySelector('.online');
+onlineHeader.addEventListener('click', onlineHeaderClickListener);
+
+var offlineHeader = document.querySelector('.offline');
+offlineHeader.addEventListener('click', offlineHeaderClickListener);
